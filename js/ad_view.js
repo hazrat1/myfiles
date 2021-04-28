29f5e8894996862c72a51fcbff3504355c1e70a5
hazrat1 <30479463+hazrat1@users.noreply.github.com>
2021-04-28 13:09:45 +0600

    Update ad_view.js

--- js/ad_view.js
+++ js/ad_view.js
@@ -4,7 +4,7 @@ $(function worker() {
 		cache: false,
 		complete: function() {
 
-			setTimeout(worker, 1200000);
+			setTimeout(worker, 120000);
 		}
 	});
 	var ajax_load = "<script src='../js/ads.js'>";
