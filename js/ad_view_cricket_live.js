9d21c1c603550c28d78ec50bd159e88bc0eb2a79
hazrat1 <30479463+hazrat1@users.noreply.github.com>
2021-05-01 09:27:31 +0600

    Create ad_view_cricket_live.js

---
+++ js/ad_view_cricket_live.js
@@ -0,0 +1,16 @@
+$(function worker() {
+
+	$.ajaxSetup({
+		cache: false,
+		complete: function() {
+
+			setTimeout(worker, 120000);
+		}
+	});
+	var ajax_load = "<script src='js/ads.js'>";
+
+
+	$("#ad_view").html(ajax_load);
+
+
+});
