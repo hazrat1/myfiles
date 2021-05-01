837597566c61910dec8feb32e60d1eab7a3faa0f
hazrat1 <30479463+hazrat1@users.noreply.github.com>
2021-05-01 15:21:05 +0600

    Update ad_view_cricket_live.js

--- js/ad_view_cricket_live.js
+++ js/ad_view_cricket_live.js
@@ -4,7 +4,7 @@ $(function worker() {
 		cache: false,
 		complete: function() {
 
-			setTimeout(worker, 120000);
+			setTimeout(worker, 300000);
 		}
 	});
 	var ajax_load = "<script src='js/ads_score.js'>";
